1ec52a25c87413df3316d794b28c5fd729b514d5
chabeli <chapibroque@gmail.com>
2022-07-22 00:57:20 -0400

    adding the pledge of honesty, Oath dialog

--- DrawingApplication/Program.cs
+++ DrawingApplication/Program.cs
@@ -16,7 +16,7 @@ namespace DrawingApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MDIParent1());
         }
     }
 }
